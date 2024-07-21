2d635367e6aedb499087017771a5ca380f5a8b1e
Tiago <tiago.mlima@hotmail.com>
2024-07-21 14:42:43 -0300

    fix

--- packages/hardhat/contracts/CBCoin.sol
+++ packages/hardhat/contracts/CBCoin.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CBCoin is ERC20, Ownable {
-    constructor() ERC20("CB Coin", "CBC") Ownable(msg.sender) {
-        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint initial supply to the owner
+    constructor() ERC20("CB Coin", "CBC") {
+        _mint(msg.sender, 1000000 * 10 ** decimals()); 
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
