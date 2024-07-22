b3d8c9005ae72d47018decafc4c23ffd2f29efd1
paollapc <paolla@usp.br>
2024-07-22 11:50:18 -0300

    ajuste pagina sobre

--- packages/nextjs/pages/sobre.tsx
+++ packages/nextjs/pages/sobre.tsx
@@ -11,45 +11,45 @@ const Sobre: React.FC = () => {
         <div className="section">
           <div className="section-text">
             <p>
-              Em um contexto global tanto referente ao consenso científico através dos dados do IPCC (Painel
-              Intergovernamental de Mudanças Climáticas), quanto aos próprios eventos climáticos extremos acontecendo
-              com mais frequência por todo o globo, graças a média da temperatura terrestre ter completado 1 ano acima
-              de 1,5°C, evidenciam que as mudanças climáticas são reais até mesmo para os negacionista de plantão.
+              Em tempos de crises e adversidades, a solidariedade e a união do povo são mais importantes do que nunca. O
+              projeto Corrente do Bem é uma plataforma descentralizada que visa resolver os gargalos da cadeia de
+              suprimentos humanitária através de certificação, rastreabilidade e transparência. Nossa missão é garantir
+              que as doações cheguem de forma eficiente e rápida às ONGs necessitadas em toda a América Latina.
             </p>
             <br />
             <p>
-              Há 15 meses os oceanos estão batendo recordes de temperaturas máximas e junho bateu o 13° recorde mensal
-              seguido de temperatura. Quando olhamos para o sul global, vemos eventos extremos climáticos acontecendo de
-              norte ao sul do Brasil. Desde seca na Amazônia, incêndios no pantanal até inundações no Acre e no RS sendo
-              intensificadas pelos fenômenos já conhecidos do El nino e La nina.
+              A América Latina enfrenta diversos desafios, desde desastres naturais até crises econômicas. Nesses
+              momentos críticos, é essencial que as comunidades se unam para apoiar aqueles que mais precisam. Nossa
+              plataforma utiliza tecnologia blockchain para assegurar que cada doação seja rastreada e chegue ao seu
+              destino de maneira transparente e segura.
             </p>
             <br />
             <p>
-              Sendo assim, podemos começar a entender que a causa raiz de todas as catástrofes globais, como as ocorrida
-              no RS-Brasil são consequências dos altos níveis de uso de combustíveis fósseis e aumento da emissão dos
-              gases de efeito estufa na terra. Já se sabe que pelo menos 30% dos brasileiros já tiveram prejuízos em
-              casa devido ao clima e 3 milhões de pessoas foram impactadas pelas inundações recentes nos RS, atingido
-              inclusive 15 mil animais que foram abandonados frente a esse desastre.
+              Através do nosso sistema, as ONGs podem criar demandas específicas de produtos necessários, e os doadores
+              podem contribuir suprindo a demanda. Cada doação é recompensada com tokens CBC, incentivando a
+              generosidade e aumentando a circulação dos tokens. Com isso, não só ajudamos a suprir necessidades
+              imediatas, mas também promovemos um ciclo contínuo de apoio e solidariedade entre os países
+              latino-americanos.
             </p>
             <br />
             <p>
-              Com esse panorama, o nosso projeto nessa plataforma busca solucionar os gargalos na cadeia de suprimentos
-              de doações em casos de emergências e situações de vulnerabilidade não só no Brasil mas em toda América
-              Latina, com transparência e certificações assegurando que as doações encontrem o caminho mais rápido e
-              eficaz de chegar a destino final que solicitar uma demanda específica de produto.
+              A união do povo latino é fundamental para enfrentar períodos de crise. Juntos, podemos fazer a diferença e
+              garantir que a ajuda chegue onde é mais necessária. Com transparência, certificação e rastreabilidade,
+              mostramos que as barreiras geográficas e linguísticas não são obstáculos para a solidariedade e o apoio
+              mútuo.
             </p>
           </div>
           <br />
-          <Image src="/voluntarios.jpg" alt="Voluntários" width={500} height={300} />
+          <Image src="/fundo.png" alt="fundo" width={500} height={300} />
         </div>
         <div className="section">
           <div className="section-text">
             <br />
             <p>
-              Visando sanar situações emergenciais e carência de recurso, o nosso diferencial é entregar certificação,
-              transparência e rastreabilidade durante toda a cadeia de suprimentos de doação de diferentes produtos.
-              Além de otimizar as entregas de doações de forma rápida, eficaz e sem desperdícios. Mostrando que a
-              fronteira da língua não é o suficiente para parar a vontade do povo latino de se ajudar.
+              Nosso diferencial é entregar certificação, transparência e rastreabilidade durante toda a cadeia de
+              suprimentos de doações. Otimizamos as entregas de forma rápida, eficaz e sem desperdícios, garantindo que
+              cada contribuição faça a diferença. A fronteira da língua não é suficiente para parar a vontade do povo
+              latino de se ajudar. Juntos, construímos uma rede de apoio sólida e eficaz em tempos de necessidade.
             </p>
             <br />
           </div>
